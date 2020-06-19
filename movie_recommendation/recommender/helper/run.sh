de1b497d6b4091f67851064cff895fced9c47217
anandanimesh98 <anandanimesh2017@gmail.com>
2020-06-19 20:42:46 +0530

    final commit

---
+++ movie_recommendation/recommender/helper/run.sh
@@ -0,0 +1,3 @@
+python3 /home/animesh/WTA/movie_recommendation/recommender/trainer/test.py
+g++ /home/animesh/WTA/movie_recommendation/recommender/trainer/process.cpp
+chmod 755 /home/animesh/WTA/movie_recommendation/recommender/trainer/a.out
---
+++ movie_recommendation/testhostA/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/animesh/WTA/movie_recommendation/testhostA/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
